291c213549bebeb1c1df75df92cdb6f47b4a4d0e
procodez <44534433+procodez@users.noreply.github.com>
2019-02-02 12:54:51 -0600

    Add files via upload

--- game.py
+++ game.py
@@ -1,17 +1,24 @@
 import random
 i = 1
-L = ["Word", "Part", "Parts"]
+L = ["dogs", "part", "lego"]
 w = random.choice(L)
+s = ['-', '-', '-', '-']
 print(len(w), "letters")
 while i <= 5:
-    g = input("Enter here: ")
-    if w == g:
-        print("yay")
-        break
-    else:
+    g = input("Enter here a letter here: ")
+    f = w.find(g)
+    if f == -1:
         i += 1
+        print("Your letter isn't here")
+    else:
+        s[f] = g
+        for items in s:
+            print(items, end=" ")
+        if '-' not in s:
+            #print(items, end=" ")
+            print("\nYou WIN")
+            quit()
 if i == 6:
     print("Ran Out Of Tries")
-    print("Your word was " + w)
-else:
-    print("That's All Folks")
+    print("The word was " + w)
+
